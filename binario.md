1aff66c507ed69556d9991b4033dfc8ec2b4e3cb
jk-514 <153434216+jk-514@users.noreply.github.com>
2024-06-20 10:09:25 +0200

    Add files via upload

---
+++ binario.md
@@ -0,0 +1,13 @@
+## Binario
+
+Binairo wird auf einem rechteckigem Feld gespielt. Einige Felder sind bereits mit schwarzen bzw. 
+weißen Kreisen gefüllt, die restlichen sind leer. Das Ziel ist es alle Felder mit Kreisen zu befüllen, sodass:
+
+1. jede Zeile/Spalte die gleiche Anzahl an schwarzen und weißen Kreisen enthält.
+2. nie mehr als zwei Kreise derselben Farbe aufeinander folgen.
+3. jede Zeile/Spalte einzigartig ist.
+
+Siehe: https://de.puzzle-binairo.com
+
+Wir gehen davon aus, dass Spielfelder quadratisch sind. Ignoriere die 3. Regel und löse Binario mit
+Backtracking.
---
+++ dictionaries.py
@@ -0,0 +1,27 @@
+groceries = {'bananas': 5, 'oranges': 3, 'apples': 4, 'milk': 2}
+
+# leeres Dictionary erstellen: {} oder dict()
+
+# groceries.clear()
+
+# print(groceries.get("oranges", 0))
+# print(groceries.get("water", 0))
+# print(groceries["water"])  # Error: water ist nicht in groceries
+
+# print(groceries.items())
+# print(groceries.keys())
+# print(groceries.values())
+
+# huebsch visualisieren
+
+# for key, value in groceries.items():
+#     print(key, value)
+
+# groceries.pop("milk")
+# print(groceries)
+# groceries.pop("cheese")  # Error: cheese ist nicht in groceries
+
+# Werte aktualisieren
+
+# groceries["water"] = 1
+# print(groceries["water"])  # Hier dann kein Error, da water hinzugefuegt wurde

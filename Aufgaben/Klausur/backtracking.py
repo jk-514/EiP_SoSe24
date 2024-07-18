d6cccfdeb3477ab5c48fb91a79d57a4a748b956d
jk-514 <153434216+jk-514@users.noreply.github.com>
2024-07-18 09:10:19 +0200

    -

---
+++ Aufgaben/Klausur/backtracking.py
@@ -0,0 +1,21 @@
+from typing import List, Tuple
+
+
+def is_complete(config: List[List[int]]) -> bool:
+    pass
+
+
+def count_1(config: List[List[int]]) -> int:
+    # Nützliche Hilfsfunktion, aber kein Muss
+    pass
+
+
+def is_allowed(config: List[List[int]]) -> bool:
+    pass
+
+
+def complete(org_list: List[List[int]], config: List[List[int]]) -> Tuple[bool, List[List[int]]]:
+    pass
+
+
+print(complete([[0, 1, 1, 0], [1, 0, 0, 0], [1, 1, 1, 0], [0, 0, 0, 1]], [])[1])

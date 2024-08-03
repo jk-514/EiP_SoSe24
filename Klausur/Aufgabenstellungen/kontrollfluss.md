619aa288b71e5465bc27dcf84a8d433b69de60d7
jk-514 <153434216+jk-514@users.noreply.github.com>
2024-08-03 16:14:43 +0200

    -

---
+++ Klausur/Aufgabenstellungen/kontrollfluss.md
@@ -0,0 +1,2 @@
+In der Datei kontrollfluss.py finden Sie drei Funktionen. \
+Ihre Aufgabe besteht darin, die entsprechenden Kontrollflussgraphen zu zeichnen.
---
+++ Klausur/resources/kontrollfluss.py
@@ -0,0 +1,32 @@
+def func1(a: int, b: int) -> int:
+    c = 1
+    while a < b:
+        if a % 2 == 0:
+            c += 1
+        else:
+            c *= 2
+
+    return c
+
+
+def func2(a: int, b: int, c: int) -> int:
+    while a < b:
+        a += 1
+        if a % 2 == 0:
+            c += 1
+        else:
+            c *= 2
+
+    return c
+
+
+def func3(a: int, b: int, c: int) -> int:
+    if a > b:
+        return c
+    if a + b > c:
+        c += 1
+    else:
+        c *= 2
+
+    c *= 3
+    return c - a + b
